1d0213e96867930808e0997f32e10c714e244537
danhickok <danhickok@gmail.com>
2021-03-28 13:31:30 -0700

    Initial

---
+++ For .NET Framework 4.7/PlayWave/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Media;
+
+namespace PlayWave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No .WAV file given");
+                return;
+            }
+
+            try
+            {
+                using (var player = new SoundPlayer())
+                {
+                    player.SoundLocation = args[0];
+                    player.PlaySync();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+    }
+}
